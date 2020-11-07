a72b7754dd65cc7d3a4757cdcb3ac81b22b395dc
madelinemc <madeline.coons@gmail.com>
2020-11-06 18:07:19 -0600

    Done.

--- my_server.rb
+++ my_server.rb
@@ -7,3 +7,7 @@ class MyServer
         (Time.now.to_i % 2).zero? ? ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
     end
 end
+
+#this way honors the way most Rack applications are built: the server setup code
+#lives in the rackup file and our application logic lives in a class that's referenced 
+#by the run command in the rackup file.
